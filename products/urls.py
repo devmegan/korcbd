5a8995d958906c44b0a3e82eb81112fe3f638e9c
mvmolloy <mvamolloy@gmail.com>
2020-09-20 08:59:37 +0000

    added product detail page and product search

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import products
+from .views import products, product_detail, search_products
+
 
 urlpatterns = [
     path('', products, name='products'),
+    path('search/', search_products, name='search_products'),
+    path('product-detail/<pk>', product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -15,3 +16,34 @@ def products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+def search_products(request):
+    query_term = None
+    products = Product.objects.all()
+
+    # handle blog searches by user
+    if request.GET:
+        if 'search_q' in request.GET:
+            query_term = request.GET['search_q']
+            if not query_term:
+                messages.error(request, "You didn't enter any search terms")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query_term) | Q(description__icontains=query_term) | Q(sku__icontains=query_term)
+            query_products = products.filter(queries)
+        context = {
+            'products': query_products,
+            'query_term': query_term,
+        }
+        return render(request, 'products/products.html', context)
+
+def product_detail(request, pk):
+    """
+    view returns details product page
+    """
+
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
