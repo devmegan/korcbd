3eb83bc37046762ef5678df7748b3587fbd6c63a
mvmolloy <mvamolloy@gmail.com>
2020-09-21 10:54:12 +0000

    added search by term/category functionality

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import products, product_detail, search_products
+from .views import products, product_detail
 
 
 urlpatterns = [
     path('', products, name='products'),
-    path('search/', search_products, name='search_products'),
     path('product-detail/<pk>', product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,32 +11,32 @@ def products(request):
     view returns products page and
     handles search/sorting queries
     """
-
     products = Product.objects.all()
-    context = {
-        'products': products,
-    }
-    return render(request, 'products/products.html', context)
-
-def search_products(request):
     query_term = None
-    products = Product.objects.all()
+    categories = None
 
     # handle blog searches by user
     if request.GET:
-        if 'search_q' in request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        elif 'search_q' in request.GET:
             query_term = request.GET['search_q']
             if not query_term:
                 messages.error(request, "You didn't enter any search terms")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query_term) | Q(description__icontains=query_term) | Q(sku__icontains=query_term)
-            query_products = products.filter(queries)
-        context = {
-            'products': query_products,
-            'query_term': query_term,
-        }
-        return render(request, 'products/products.html', context)
+            products = products.filter(queries)
+    context = {
+        'products': products,
+        'query_term': query_term,
+        'current_categores': categories,
+    }
+    return render(request, 'products/products.html', context)
+
 
 def product_detail(request, pk):
     """
