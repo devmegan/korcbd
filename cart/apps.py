d381b12c08ffe3fbbc1256e930eb61a860f41c8d
mvmolloy <mvamolloy@gmail.com>
2020-09-24 14:49:17 +0000

    added signal to update order total

--- cart/apps.py
+++ cart/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class CartConfig(AppConfig):
     name = 'cart'
+
+    def ready(self):
+        """ import signals so signals module/listeners work """
+        import cart.signals
---
+++ cart/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@reciever(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """ update order total on creating/ updating line items """
+
+    instance.order.update_total()
+
+
+@reciever(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, created, **kwargs):
+    """ update order total on creating/ deleting line items """
+
+    instance.order.update_total()
