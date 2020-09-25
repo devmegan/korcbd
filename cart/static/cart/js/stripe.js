9bcc863b5c3cab88342177e515275096d48beaa6
mvmolloy <mvamolloy@gmail.com>
2020-09-25 14:37:38 +0000

    added js to handle stripe payment submission

--- cart/static/cart/js/stripe.js
+++ cart/static/cart/js/stripe.js
@@ -42,3 +42,37 @@ card.addEventListener('change', function (event) {
         errorDiv.textContent = '';
     }
 });
+
+// Handle submitting the payment form
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    // prevent user resubmitting successful payment while confirmation page loads
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // display any payment errors to user
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // reenable card/submit elements so user can resubmit form
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                // submit form if payment succeeds
+                form.submit();
+            }
+        }
+    });
+});
