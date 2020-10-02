2ac3b794adb71dba4d7683a784c4e2058b121cda
mvmolloy <mvamolloy@gmail.com>
2020-10-02 13:35:38 +0000

    updated checkout form elements

--- cart/forms.py
+++ cart/forms.py
@@ -8,7 +8,7 @@ class OrderForm(forms.ModelForm):
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
-                  'county', 'paid', 'order_total')
+                  'county',)
 
     def __init__(self, *args, **kwargs):
         """ placeholders for form fields """
@@ -17,25 +17,23 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Post Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
-            'paid': 'Paid',
-            'order_total': 'Order_Total',
+            'county': 'County/State/Province',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                # add asterisk to required fields
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            # set values for fields
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    # add asterisk to required fields
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # set values for fields
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # style fields to match stripe element
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             # remove form field labels
