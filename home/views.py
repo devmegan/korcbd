62062f005ba339a53b350ffae81f2dc79f58f736
mvmolloy <mvamolloy@gmail.com>
2020-10-04 21:44:54 +0000

    passing blog posts to home template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 
+
 def index(request):
     """ view returns index page """
-    return render(request, 'home/index.html')
+    blog_posts = Post.objects.all().order_by('id')[:3]
+    context = {
+        'blog_posts': blog_posts,
+    }
+    return render(request, 'home/index.html', context)
