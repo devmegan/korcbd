1e33f6f8f2e9a3b5cb66b13bd8d46c11a89bb9b8
mvmolloy <mvamolloy@gmail.com>
2020-10-06 08:46:27 +0000

    wiring up s3 aws static/media files config

--- korcbd/settings.py
+++ korcbd/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'profiles',
+    # other
+    'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -176,6 +179,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS':
+    # AWS S3 Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'korcbd'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # static/media urls for production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
 
 # Stripe Settings
 STRIPE_CURRENCY = 'gbp'
