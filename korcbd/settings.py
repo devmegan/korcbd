7e931797515206ef6ddba72c2439a4f4966d5133
mvmolloy <mvamolloy@gmail.com>
2020-10-07 15:05:02 +0000

    added deployed email settings

--- korcbd/settings.py
+++ korcbd/settings.py
@@ -110,8 +110,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -212,4 +210,13 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '-_-')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '-_-')
 
 # Email Settings
-DEFAULT_FROM_EMAIL = 'korcbdproducts@gmail.com'
+if 'DEVELOPMENT' in os.environ:
+    DEFAULT_FROM_EMAIL = 'korcbdproducts@gmail.com'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PW = os.environ.get('EMAIL_HOST_PW')
