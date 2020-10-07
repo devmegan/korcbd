fab011b5f2ac805202fb9f0013e5d620288241fd
mvmolloy <mvamolloy@gmail.com>
2020-10-07 15:26:57 +0000

    updating smtp email settings

--- korcbd/settings.py
+++ korcbd/settings.py
@@ -219,4 +219,4 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PW = os.environ.get('EMAIL_HOST_PW')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PW')
