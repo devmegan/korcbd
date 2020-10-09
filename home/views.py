0936aaaa0f6d20c337a6defd4f6053e7cb52911c
mvmolloy <mvamolloy@gmail.com>
2020-10-09 11:19:42 +0000

    passing profile to home template to populate contact form

--- home/views.py
+++ home/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from blog.models import Post
+from profiles.models import UserProfile
 
 # Create your views here.
 
 
-def index(request):
+def index(request): 
+    profile = None
+    blog_posts = None
     """ view returns index page """
     blog_posts = Post.objects.all().order_by('id')[:3]
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        
     context = {
         'blog_posts': blog_posts,
+        'profile': profile,
     }
     return render(request, 'home/index.html', context)
 
