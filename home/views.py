8ebdb65240a3c0dce8f05299a14d75a68c1e764c
mvmolloy <mvamolloy@gmail.com>
2020-10-13 20:56:14 +0000

    fixing bare except error with condition

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
+# from django.core.exceptions import DoesNotExist
 from django.contrib import messages
-from profiles.models import UserProfile
 from cart.models import Order
 
 # Create your views here.
@@ -10,21 +10,17 @@ from cart.models import Order
 def index(request):
     """ view returns index page """
 
-    profile = None
     blog_posts = None
     track_order = None
 
     blog_posts = Post.objects.all().order_by('id')[:3]
-
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
 
     # handle order tracking requests
     if request.POST:
         order_ref = request.POST['order-ref-track']
         try:
             track_order = Order.objects.get(order_reference=order_ref)
-        except:
+        except Order.DoesNotExist:
             messages.error(
                 request,
                 f"{order_ref} doesn't match an order placed with us. \
@@ -34,7 +30,6 @@ def index(request):
 
     context = {
         'blog_posts': blog_posts,
-        'profile': profile,
         'track_order': track_order,
     }
     return render(request, 'home/index.html', context)
