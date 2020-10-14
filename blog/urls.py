3ca4fdc962c58f77c67b404b0083cd80b7b55860
mvmolloy <mvamolloy@gmail.com>
2020-10-14 15:10:48 +0000

    adding trailing '/' to urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,19 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.Blog.as_view(), name='blog'),
-    path('blog_post/<int:pk>', views.blogPost.as_view(), name='blog_post'),
+    path('blog_post/<int:pk>/', views.blogPost.as_view(), name='blog_post'),
     path('search/', views.searchPosts, name='search'),
     path('add_post/', views.addPost.as_view(), name='add_post'),
-    path('edit_post/<int:pk>', views.editPost.as_view(), name='edit_post'),
+    path('edit_post/<int:pk>/', views.editPost.as_view(), name='edit_post'),
     path(
-        'delete_post/<int:pk>',
+        'delete_post/<int:pk>/',
         views.deletePost.as_view(),
         name='delete_post'),
-    path('tag/<str:tag_q>', views.filterPosts, name='tag'),
-    path('heart_post/<int:pk>', views.heartPost, name='heart_post'),
-    path('comment_post/<int:pk>', views.commentPost, name='add_comment'),
+    path('tag/<str:tag_q>/', views.filterPosts, name='tag'),
+    path('heart_post/<int:pk>/', views.heartPost, name='heart_post'),
+    path('comment_post/<int:pk>/', views.commentPost, name='add_comment'),
     path(
-        'delete_comment/<int:pk>',
+        'delete_comment/<int:pk>/',
         views.deleteComment,
         name='delete_comment'
     ),
