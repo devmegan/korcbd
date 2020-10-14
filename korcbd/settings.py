842f35283b24420d2e6fa13e9e57fd6567709104
mvmolloy <mvamolloy@gmail.com>
2020-10-14 14:24:57 +0000

    fixing pep8 formatting error with auth password validators

--- korcbd/settings.py
+++ korcbd/settings.py
@@ -143,21 +143,23 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation\
-        .UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.\
-        MinimumLengthValidator',
+        'django.contrib.auth.password_validation.' +
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.' +
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.' +
+        'NumericPasswordValidator',
     },
 ]
 
