4257bb2d3630824bd98d9e5ca05573728e7319c4
mvmolloy <mvamolloy@gmail.com>
2020-10-15 15:36:13 +0000

    added extra tests for blog comment/hearts crud functionality

--- blog/test_views.py
+++ blog/test_views.py
@@ -77,8 +77,41 @@ class BlogViewsTestsLoggedIn(TestCase):
             username='hjansen', email='hj@email.com', password='ikhebkaas42'
         )
 
+
+
+class BlogPostViewsTestsLoggedOut(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='hjansen', email='hj@email.com', password='ikhebkaas42'
+        )
+
+    def test_response_200(self):
+        post = Post.objects.create(
+            title="Test Post Title",
+            body="Test post body",
+            author=self.user,
+            tag_1='tag1',
+            tag_2='tag2',
+            tag_3='tag3',
+        )
+        """ test loading blog home page successful """
+        response = self.client.get(f'/blog/blog_post/{post.id}/')
+        context = response.context
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(context['post'], post)
+
+
+class BlogPostViewsTestsNonSuperuserLoggedIn(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='hjansen', email='hj@email.com', password='ikhebkaas42'
+        )
+
     def test_nonsuperuser_can_create_comment(self):
-        """ check nonsuperuser can create comment """
+        """ check nonsuperuser can create comment and is
+        redirected back to blog post """
         post = Post.objects.create(
             title="Test Post Title",
             author=self.user,
@@ -87,20 +120,71 @@ class BlogViewsTestsLoggedIn(TestCase):
             tag_2='tag2',
             tag_3='tag3',
         )
-        post_id = int(post.id)
         client = Client()
         client.login(username='hjansen', password='ikhebkaas42')
-        # Create an instance of a GET request.
-        client.post(
-            f'/blog/comment_post/{post_id}',
+        response = client.post(
+            f'/blog/comment_post/{post.id}/',
             {
-                'author': self.user,
+                'post_to_comment_id': post.id,
+                'author': self.user.id,
                 'comment_body': 'Test comment body',
-
             }
         )
-        print(post.comments)
-        # self.assertEqual(response.status_code, 301)
+        comment_created = Comment.objects.filter(author=self.user)
+        self.assertTrue(comment_created)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, f"/blog/blog_post/{post.id}/")
+
+    def test_nonsuperuser_can_delete_own_comments(self):
+        """ check nonsuperuser can create comment and is
+        redirected back to blog post """
+        post = Post.objects.create(
+            title="Test Post Title",
+            author=self.user,
+            body="Test post body",
+            tag_1='tag1',
+            tag_2='tag2',
+            tag_3='tag3',
+        )
+        comment = Comment.objects.create(
+            author=self.user,
+            post_to_comment=post,
+            comment_body='Test comment body'
+        )
+        client = Client()
+        client.login(username='hjansen', password='ikhebkaas42')
+        response = client.post(
+            f'/blog/delete_comment/{post.id}/',
+            {
+                'comment_id': comment.id
+            }
+        )
+        comment_exists = Comment.objects.filter(author=self.user)
+        self.assertFalse(comment_exists)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, f"/blog/blog_post/{post.id}/")
+
+    def test_nonsuperuser_can_heart_a_post(self):
+        """ check nonsuperuser can create comment and is
+        redirected back to blog post """
+        client = Client()
+        client.login(username='hjansen', password='ikhebkaas42')
+        post = Post.objects.create(
+            title="Test Post Title",
+            author=self.user,
+            body="Test post body",
+            tag_1='tag1',
+            tag_2='tag2',
+            tag_3='tag3',
+        )
+        client = Client()
+        client.login(username='hjansen', password='ikhebkaas42')
+        response = client.get(f'/blog/heart_post/{post.id}/')
+        post = Post.objects.get(id=post.id)
+        self.assertTrue(post.hearts)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, f"/blog/blog_post/{post.id}/")
+
 
 # testing helper functions
 def fetch_template_names(templates):
